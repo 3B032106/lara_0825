585cdfa0c6280cf4dfd2d8c521bb513e8ff2f9be
李宣佑 <3B032106@gm.student.ncut.edu.tw>
2023-11-03 14:31:04 +0800

    0825_u2_p2: 用save()方法新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(route('posts.index'));
+    //return redirect(route('posts.index'));
+    $post = new Post();
+    $post->title="test_title";
+    $post->content="test_content";
+    $post->save();
+    return 'Saved, OK!';
 });
 
 Route::get('posts',[PostController::class, 'index'])->name('posts.index');
